02d16ec47ee5b2d7ee4594808fe95f051a1ba342
nagmabano <bano.nagma.1@gmail.com>
2018-09-06 18:54:52 +0530

    inline template

--- my-app/src/app/header/header.component.ts
+++ my-app/src/app/header/header.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
-  templateUrl: './header.component.html',
+  template: `<h1>Displayed using template property.</h1>
+              <p>Its been written over multiple lines.</p>`,
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
