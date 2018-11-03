2a5a8ff7334e428e2f819b79c2b5250de49bc0b6
nagmabano <bano.nagma.1@gmail.com>
2018-11-03 18:17:32 +0530

    data communication from parent to child component

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { ParentComponent } from './parent/parent.component';
+import { ChildComponent } from './child/child.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    ParentComponent,
+    ChildComponent
   ],
   imports: [
     BrowserModule,
